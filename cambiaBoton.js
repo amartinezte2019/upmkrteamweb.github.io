ef77d02aa57bf786c205cd54b99cef672c7b9672
amartinezte2019 <99871385+amartinezte2019@users.noreply.github.com>
2023-10-20 12:34:22 +0200

    implementacion botón desplegable "más integrantes del equipo"

---
+++ cambiaBoton.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Obtener los elementos del DOM
+const boton = document.querySelector('#boton01');
+const marcoFoto = document.querySelector('#marco_foto');
+
+// Establecer el estado inicial del botón
+let mostrarFoto = false;
+actualizarBoton();
+
+// Agregar el listener para el botón
+boton.addEventListener('click', function() {
+// Cambiar el estado del botón
+mostrarFoto = !mostrarFoto;
+// Actualizar el botón
+actualizarBoton();
+// Mostrar u ocultar la foto según el estado del botón
+if (mostrarFoto) {
+marcoFoto.classList.remove('oculto');
+} else {
+marcoFoto.classList.add('oculto');
+}
+});
+
+// Función para actualizar el texto del botón según el estado
+function actualizarBoton() {
+if (mostrarFoto) {
+boton.textContent = 'ocultar resto del equipo';
+} else {
+boton.textContent = 'Ver el resto del equipo';
+}
+}
